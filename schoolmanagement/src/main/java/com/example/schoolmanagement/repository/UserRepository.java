ebeda228587108656c15dc74d80a5aac31111a2d
BuildTools <unconfigured@null.spigotmc.org>
2021-11-22 12:46:22 +0800

    fixed design bug from using spring security

---
+++ schoolmanagement/src/main/java/com/example/schoolmanagement/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.schoolmanagement.repository;
+
+import com.example.schoolmanagement.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
